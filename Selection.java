7deb4759c3f2d41eb36e5e40d753eec4ce3b4c0e
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2023-06-06 23:25:34 +0530

    Add files via upload
    Flow control in if - else  Selection statement in java
    if (  )   statement takes only boolean type as argument other than boolean it will prompt for a compile time error i.e ... incompatible types

---
+++ Selection.java
@@ -0,0 +1,16 @@
+class Selection
+{
+    public static void main(String []argh)
+    {
+		int x=14;
+		if (x==14)
+		{
+			System.out.println("Hello.... Mr"+x);
+		}
+		else
+		{
+			System.out.println("Hi.... Mr"+x);
+		}
+	}
+       
+}
